1cebb5524d79e5f591bd7501c6a94b7324750011
kdd6826 <67967689+kdd6826@users.noreply.github.com>
2022-05-09 12:59:55 +0900

    save

--- Source/UnrealTut/Fountain.cpp
+++ Source/UnrealTut/Fountain.cpp
@@ -11,13 +11,13 @@
 AFountain::AFountain()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 	Body = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BODY"));
 	Water = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("WATER"));
 	Light = CreateDefaultSubobject<UPointLightComponent>(TEXT("LIGHT"));
 	Splash = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("SPLASH"));
-
+	Movement = CreateDefaultSubobject<URotatingMovementComponent>(TEXT("MOVEMENT"));
 
 	RootComponent = Body;
 	Water->SetupAttachment(Body);
@@ -48,7 +48,8 @@ AFountain::AFountain()
 		Splash->SetTemplate(PS_SPLASH.Object);
 	}
 	
-	
+	RotateSpeed = 30.f;
+	Movement->RotationRate = FRotator(0.0f, RotateSpeed, 0.f);
 }
 
 // Called when the game starts or when spawned
@@ -77,6 +78,7 @@ void AFountain::PostInitializeComponents()
 void AFountain::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	AddActorLocalRotation(FRotator(0.f, RotateSpeed * DeltaTime, 0.f));
 	//Tess Git
 }
 
--- Source/UnrealTut/Fountain.h
+++ Source/UnrealTut/Fountain.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "UnrealTut.h"
+#include "GameFramework/RotatingMovementComponent.h"
 #include "GameFramework/Actor.h"
 #include "Fountain.generated.h"
 
@@ -38,4 +39,11 @@ public:
 
 	UPROPERTY(EditAnywhere, Category=ID)
 	int32 ID;
+
+	UPROPERTY(VisibleAnywhere)
+	URotatingMovementComponent* Movement;
+
+private:
+	UPROPERTY(EditAnywhere, Category=Stat,Meta = (AllowPrivateAccess = true))
+	float RotateSpeed;
 };
