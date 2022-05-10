f872c7e39b238ae9c988aad7ee72622fb104d25f
kdd6826 <67967689+kdd6826@users.noreply.github.com>
2022-05-10 15:54:54 +0900

    use character (instead pawn)

--- Source/UnrealTut/ABGameMode.cpp
+++ Source/UnrealTut/ABGameMode.cpp
@@ -2,12 +2,12 @@
 
 
 #include "ABGameMode.h"
-#include "ABPawn.h"
+#include "ABCharacter.h"
 #include "ABPlayerController.h"
 
 AABGameMode::AABGameMode()
 {
-	DefaultPawnClass = AABPawn::StaticClass();
+	DefaultPawnClass = AABCharacter::StaticClass();
 	PlayerControllerClass = AABPlayerController::StaticClass();
 	/*static ConstructorHelpers::FClassFinder<APawn> BP_PAWN_C(TEXT("/Game/ThirdPersonBP/Blueprints/ThirdPersonCharacter.ThirdPersonCharacter_C"));
 	if (BP_PAWN_C.Succeeded())
