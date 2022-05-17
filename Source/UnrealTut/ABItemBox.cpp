cd052b437dad5caac2158c5917440dd406671202
kdd6826 <67967689+kdd6826@users.noreply.github.com>
2022-05-18 01:01:05 +0900

    ~ItemBox

--- Source/UnrealTut/ABItemBox.cpp
+++ Source/UnrealTut/ABItemBox.cpp
@@ -7,8 +7,25 @@
 AABItemBox::AABItemBox()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
+	Trigger = CreateDefaultSubobject<UBoxComponent>(TEXT("TRIGGER"));
+	Box = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BOX"));
+
+	RootComponent = Trigger;
+	Box->SetupAttachment(RootComponent);
+
+	Trigger->SetBoxExtent(FVector(40.f, 42.f, 30.f));
+	
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> SM_BOX(TEXT("/Game/InfinityBladeGrassLands/Environments/Breakables/StaticMesh/Box/SM_Env_Breakables_Box1.SM_Env_Breakables_Box1"));
+	if (SM_BOX.Succeeded())
+	{
+		Box->SetStaticMesh(SM_BOX.Object);
+	}
+	Box->SetRelativeLocation(FVector(0.f, -3.5f, -30.f));
+
+	Trigger->SetCollisionProfileName(TEXT("ItemBox"));
+	Box->SetCollisionProfileName(TEXT("NoCollision"));
 }
 
 // Called when the game starts or when spawned
@@ -18,10 +35,16 @@ void AABItemBox::BeginPlay()
 	
 }
 
-// Called every frame
-void AABItemBox::Tick(float DeltaTime)
+void AABItemBox::PostInitializeComponents()
 {
-	Super::Tick(DeltaTime);
-
+	Super::PostInitializeComponents();
+	Trigger->OnComponentBeginOverlap.AddDynamic(this, &AABItemBox::OnCharacterOverlap);
 }
+
+void AABItemBox::OnCharacterOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	ABLOG_S(Warning);
+}
+
+
 
--- Source/UnrealTut/ABItemBox.h
+++ Source/UnrealTut/ABItemBox.h
@@ -2,7 +2,7 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
+#include "UnrealTut.h"
 #include "GameFramework/Actor.h"
 #include "ABItemBox.generated.h"
 
@@ -18,9 +18,17 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	virtual void PostInitializeComponents() override;
 
 public:	
 	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	UFUNCTION()
+		void OnCharacterOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+			int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
+	UPROPERTY(VisibleAnywhere,Category=Box)
+	UBoxComponent* Trigger;
+
+	UPROPERTY(VisibleAnywhere, Category = Box)
+	UStaticMeshComponent* Box;
 };
