affaddf080bdf1f816d5079ccd80aa042c9b3169
kdd6826 <67967689+kdd6826@users.noreply.github.com>
2022-05-28 20:12:39 +0900

    Section - Door

---
+++ Source/UnrealTut/Private/ABSection.cpp
@@ -0,0 +1,127 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ABSection.h"
+
+// Sets default values
+AABSection::AABSection()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MESH"));
+	RootComponent = Mesh;
+
+	FString AssetPath = TEXT("/Game/Book/StaticMesh/SM_SQUARE.SM_SQUARE");
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> SM_SQUARE(*AssetPath);
+	if (SM_SQUARE.Succeeded())
+	{
+		Mesh->SetStaticMesh(SM_SQUARE.Object);
+	}
+	else
+	{
+		ABLOG(Error, TEXT("Failed to load staticmesh asset. : %s"), *AssetPath);
+	}
+	
+	Trigger = CreateDefaultSubobject<UBoxComponent>(TEXT("TRIGGER"));
+	Trigger->SetBoxExtent(FVector(775.f, 775.f, 300.f));
+	Trigger->SetupAttachment(RootComponent);
+	Trigger->SetRelativeLocation(FVector(0.f, 0.f, 250.f));
+	Trigger->SetCollisionProfileName(TEXT("ABTrigger"));
+
+	FString GateAssetPath = TEXT("/Game/Book/StaticMesh/SM_GATE.SM_GATE");
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> SM_GATE(*GateAssetPath);
+	if (!SM_GATE.Succeeded())
+	{
+		ABLOG(Error, TEXT("Failed to load staticmesh asset. : %s"), *GateAssetPath);
+	}
+
+	static FName GateSockets[] = { {TEXT("+XGate")},{TEXT("-XGate")},{TEXT("+YGate")},{TEXT("-YGate")} };
+	for (FName GateSocket : GateSockets)
+	{
+		ABCHECK(Mesh->DoesSocketExist(GateSocket));
+		UStaticMeshComponent* NewGate = CreateDefaultSubobject<UStaticMeshComponent>(*GateSocket.ToString());
+		NewGate->SetStaticMesh(SM_GATE.Object);
+		NewGate->SetupAttachment(RootComponent, GateSocket);
+		NewGate->SetRelativeLocation(FVector(0.f, -80.5f, 0.f));
+		GateMeshes.Add(NewGate);
+		
+		UBoxComponent* NewGateTrigger = CreateDefaultSubobject<UBoxComponent>(*GateSocket.ToString().Append(TEXT("Trigger")));
+		NewGateTrigger->SetBoxExtent(FVector(100.f, 100.f, 300.f));
+		NewGateTrigger->SetupAttachment(RootComponent, GateSocket);
+		NewGateTrigger->SetRelativeLocation(FVector(70.f, 0.f, 250.f));
+		NewGateTrigger->SetCollisionProfileName(TEXT("ABTrigger"));
+		GateTriggers.Add(NewGateTrigger);
+	}
+
+	bNoBattle = false;
+}
+
+// Called when the game starts or when spawned
+void AABSection::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	SetState(bNoBattle ? ESectionState::COMPLETE : ESectionState::READY);
+}
+
+// Called every frame
+void AABSection::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AABSection::SetState(ESectionState NewState)
+{
+	switch (NewState)
+	{
+	case AABSection::ESectionState::READY:
+	{
+		Trigger->SetCollisionProfileName(TEXT("ABTrigger"));
+		for (UBoxComponent* GateTrigger : GateTriggers)
+		{
+			GateTrigger->SetCollisionProfileName(TEXT("NoCollision"));
+		}
+
+		OperateGates(true);
+		break;
+	}
+	case AABSection::ESectionState::BATTLE:
+	{
+		Trigger->SetCollisionProfileName(TEXT("NoCollision"));
+		for (UBoxComponent* GateTrigger : GateTriggers)
+		{
+			GateTrigger->SetCollisionProfileName(TEXT("NoCollision"));
+		}
+
+		OperateGates(false);
+		break;
+	}
+		
+	case AABSection::ESectionState::COMPLETE:
+	{
+		Trigger->SetCollisionProfileName(TEXT("NoCollision"));
+		for (UBoxComponent* GateTrigger : GateTriggers)
+		{
+			GateTrigger->SetCollisionProfileName(TEXT("ABTrigger"));
+		}
+
+		OperateGates(true);
+		break;
+	}
+	
+	default:
+		break;
+	}
+	CurrentState = NewState;
+}
+
+void AABSection::OperateGates(bool bOpen)
+{
+	for (UStaticMeshComponent* Gate : GateMeshes)
+	{
+		Gate->SetRelativeRotation(bOpen ? FRotator(0.f, -90.f, 0.f) : FRotator::ZeroRotator);
+	}
+}
+
---
+++ Source/UnrealTut/Public/ABSection.h
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "UnrealTut.h"
+#include "GameFramework/Actor.h"
+#include "ABSection.generated.h"
+
+UCLASS()
+class UNREALTUT_API AABSection : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AABSection();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+private:
+	enum class ESectionState : uint8
+	{
+		READY = 0,
+		BATTLE,
+		COMPLETE
+	};
+
+	void SetState(ESectionState NewState);
+	ESectionState CurrentState = ESectionState::READY;
+
+	void OperateGates(bool bOpen = true);
+private:
+	UPROPERTY(VisibleAnywhere, Category = Mesh, Meta = (AllowPrivateAccess = true))
+	TArray<UStaticMeshComponent*> GateMeshes;
+
+	UPROPERTY(VisibleAnywhere,Category=Trigger,Meta=(AllowPrivateAccess = true))
+	TArray<UBoxComponent*> GateTriggers;
+
+	UPROPERTY(VisibleAnywhere,Category=Mesh,Meta=(AllowPrivateAccess = true))
+	UStaticMeshComponent* Mesh;
+
+	UPROPERTY(VisibleAnywhere,Category=Trigger,Meta=(AllowPrivateAccess = true))
+	UBoxComponent* Trigger;
+
+	UPROPERTY(EditAnywhere,Category = State, Meta = (AllowPrivateAccess = true))
+	bool bNoBattle;
+};
